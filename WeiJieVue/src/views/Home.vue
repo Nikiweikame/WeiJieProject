aad7c1d95332c0ce1e817d8c42a4f8454df2557d
Niki wei <frank29580649@gmail.com>
2023-04-17 14:52:01 +0800

    router path

---
+++ WeiJieVue/src/views/Home.vue
@@ -0,0 +1,301 @@
+<script setup>
+import { ref } from "vue";
+</script>
+<style lang="scss"></style>
+<template>
+  <div class="example">
+    <div class="content">
+      <div
+        id="rev_slider_1078_1_wrapper"
+        class="rev_slider_wrapper fullwidthbanner-container"
+        data-alias="classic4export"
+        data-source="gallery"
+        style="
+          margin: 0px auto;
+          background-color: transparent;
+          padding: 0px;
+          margin-top: 0px;
+          margin-bottom: 0px;
+        "
+      >
+        <!-- START REVOLUTION SLIDER 5.4.1 fullwidth mode -->
+        <div
+          id="rev_slider_1078_1"
+          class="rev_slider fullwidthabanner"
+          data-version="5.4.1"
+        >
+          <ul>
+            <!-- SLIDE  -->
+            <li
+              data-index="rs-3045"
+              data-transition="zoomout"
+              data-slotamount="default"
+              data-hideafterloop="0"
+              data-hideslideonmobile="off"
+              data-easein="Power4.easeInOut"
+              data-easeout="Power4.easeInOut"
+              data-masterspeed="2000"
+              data-thumb="../upload/slider_02.jpg"
+              data-rotate="0"
+              data-fstransition="fade"
+              data-fsmasterspeed="1500"
+              data-fsslotamount="7"
+              data-saveperformance="off"
+              data-title="Intro"
+              data-param1=""
+              data-param2=""
+              data-param3=""
+              data-param4=""
+              data-param5=""
+              data-param6=""
+              data-param7=""
+              data-param8=""
+              data-param9=""
+              data-param10=""
+              data-description=""
+            >
+              <!-- MAIN IMAGE -->
+              <img
+                src="../upload/slider_02.jpg"
+                alt=""
+                data-bgposition="center center"
+                data-bgfit="cover"
+                data-bgrepeat="no-repeat"
+                data-bgparallax="10"
+                class="rev-slidebg"
+                data-no-retina
+              />
+
+              <!-- LAYER NR. 1 -->
+              <div
+                class="tp-caption NotGeneric-Title tp-resizeme"
+                id="slide-3045-layer-133"
+                data-x="['left','left','left','center']"
+                data-hoffset="['105','105','60','0']"
+                data-y="['middle','middle','middle','middle']"
+                data-voffset="['-30','-30','-30','-25']"
+                data-fontsize="['48','48','44','25']"
+                data-lineheight="['70','70','70','50']"
+                data-width="none"
+                data-height="none"
+                data-whitespace="nowrap"
+                data-type="text"
+                data-responsive_offset="on"
+                data-frames='[{"from":"x:[105%];z:0;rX:45deg;rY:0deg;rZ:90deg;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":2000,"to":"o:1;","delay":1000,"split":"chars","splitdelay":0.05,"ease":"Power4.easeInOut"},{"delay":"wait","speed":1000,"to":"y:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power2.easeInOut"}]'
+                data-textAlign="['left','left','left','left']"
+                data-paddingtop="[10,10,10,10]"
+                data-paddingright="[0,0,0,30]"
+                data-paddingbottom="[10,10,10,10]"
+                data-paddingleft="[0,0,0,30]"
+                style="z-index: 5; white-space: nowrap; text-transform: left"
+              >
+                Innovative solutions
+              </div>
+
+              <!-- LAYER NR.2 -->
+              <div
+                class="tp-caption NotGeneric-SubTitle tp-resizeme"
+                id="slide-3045-layer-4"
+                data-x="['left','left','left','center']"
+                data-hoffset="['105','110','60','0']"
+                data-y="['middle','middle','middle','center']"
+                data-voffset="['55','55','55','40']"
+                data-width="none"
+                data-height="none"
+                data-fontsize="['18','18','18','14']"
+                data-lineheight="['31','31','31','19']"
+                data-whitespace="nowrap"
+                data-type="text"
+                data-responsive_offset="on"
+                data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","speed":2000,"to":"o:1;","delay":1500,"ease":"Power4.easeInOut"},{"delay":"wait","speed":1000,"to":"y:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power2.easeInOut"}]'
+                data-textAlign="['left','left','left','left']"
+                data-paddingtop="[0,0,0,0]"
+                data-paddingright="[0,0,0,90]"
+                data-paddingbottom="[0,0,0,0]"
+                data-paddingleft="[0,0,0,90]"
+                style="z-index: 6; white-space: nowrap; text-transform: left"
+              >
+                We find the best solutions for you, we redesign your home and
+                work places. <br />Discover why over 5,100 home owners trust
+                Initiative.
+              </div>
+
+              <!-- LAYER NR. 3 -->
+              <div
+                class="tp-caption NotGeneric-CallToAction rev-btn"
+                id="slide-1699-layer-7"
+                data-x="['left','left','left','center']"
+                data-hoffset="['105','107','60','0']"
+                data-y="['middle','middle','middle','middle']"
+                data-voffset="['144','144','144','125']"
+                data-width="none"
+                data-height="none"
+                data-whitespace="nowrap"
+                data-type="button"
+                data-actions='[{"event":"click","action":"scrollbelow","offset":"0px","delay":""}]'
+                data-responsive_offset="on"
+                data-responsive="off"
+                data-frames='[{"from":"y:50px;opacity:0;","speed":1500,"to":"o:1;","delay":1250,"ease":"Power4.easeInOut"},{"delay":"wait","speed":1000,"to":"y:[175%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power2.easeInOut"},{"frame":"hover","speed":"300","ease":"Power1.easeInOut","to":"o:1;rX:0;rY:0;rZ:0;z:0;","style":"c:rgba(255, 255, 255, 1.00);bc:rgba(255, 255, 255, 1.00);bw:1px 1px 1px 1px;"}]'
+                data-textAlign="['left','left','left','left']"
+                data-paddingtop="[15,15,15,15]"
+                data-paddingright="[40,40,40,40]"
+                data-paddingbottom="[15,15,15,15]"
+                data-paddingleft="[40,40,40,40]"
+                style="
+                  z-index: 9;
+                  color: #f7bd1f;
+                  white-space: nowrap;
+                  text-transform: left;
+                  outline: none;
+                  box-shadow: none;
+                  box-sizing: border-box;
+                  -moz-box-sizing: border-box;
+                  -webkit-box-sizing: border-box;
+                  cursor: pointer;
+                  font-family: Oswald;
+                  letter-spacing: 1px;
+                "
+              >
+                HOOK ME UP
+              </div>
+            </li>
+            <!-- SLIDE  -->
+            <li
+              data-index="rs-30451"
+              data-transition="zoomout"
+              data-slotamount="default"
+              data-hideafterloop="0"
+              data-hideslideonmobile="off"
+              data-easein="Power4.easeInOut"
+              data-easeout="Power4.easeInOut"
+              data-masterspeed="2000"
+              data-thumb="../upload/slider_01.jpg"
+              data-rotate="0"
+              data-fstransition="fade"
+              data-fsmasterspeed="1500"
+              data-fsslotamount="7"
+              data-saveperformance="off"
+              data-title="Intro"
+              data-param1=""
+              data-param2=""
+              data-param3=""
+              data-param4=""
+              data-param5=""
+              data-param6=""
+              data-param7=""
+              data-param8=""
+              data-param9=""
+              data-param10=""
+              data-description=""
+            >
+              <!-- MAIN IMAGE -->
+              <img
+                src="../upload/slider_01.jpg"
+                alt=""
+                data-bgposition="center center"
+                data-bgfit="cover"
+                data-bgrepeat="no-repeat"
+                data-bgparallax="10"
+                class="rev-slidebg"
+                data-no-retina
+              />
+
+              <!-- LAYER NR. 1 -->
+              <div
+                class="tp-caption NotGeneric-Title tp-resizeme"
+                id="slide-3045-layer-13333"
+                data-x="['left','left','left','center']"
+                data-hoffset="['105','105','60','0']"
+                data-y="['middle','middle','middle','middle']"
+                data-voffset="['-30','-30','-30','-25']"
+                data-fontsize="['48','48','44','25']"
+                data-lineheight="['70','70','70','50']"
+                data-width="none"
+                data-height="none"
+                data-whitespace="nowrap"
+                data-type="text"
+                data-responsive_offset="on"
+                data-frames='[{"from":"x:[105%];z:0;rX:45deg;rY:0deg;rZ:90deg;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":2000,"to":"o:1;","delay":1000,"split":"chars","splitdelay":0.05,"ease":"Power4.easeInOut"},{"delay":"wait","speed":1000,"to":"y:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power2.easeInOut"}]'
+                data-textAlign="['left','left','left','left']"
+                data-paddingtop="[10,10,10,10]"
+                data-paddingright="[0,0,0,0]"
+                data-paddingbottom="[10,10,10,10]"
+                data-paddingleft="[0,0,0,0]"
+                style="z-index: 5; white-space: nowrap; text-transform: left"
+              >
+                Outstanding Office's
+              </div>
+
+              <!-- LAYER NR.2 -->
+              <div
+                class="tp-caption NotGeneric-SubTitle tp-resizeme"
+                id="slide-3045-layer-4444"
+                data-x="['left','left','left','center']"
+                data-hoffset="['105','110','60','0']"
+                data-y="['middle','middle','middle','middle']"
+                data-voffset="['55','55','55','40']"
+                data-width="none"
+                data-height="none"
+                data-fontsize="['18','18','18','14']"
+                data-lineheight="['31','31','31','19']"
+                data-whitespace="nowrap"
+                data-type="text"
+                data-responsive_offset="on"
+                data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","speed":2000,"to":"o:1;","delay":1500,"ease":"Power4.easeInOut"},{"delay":"wait","speed":1000,"to":"y:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power2.easeInOut"}]'
+                data-textAlign="['left','left','left','left']"
+                data-paddingtop="[0,0,0,0]"
+                data-paddingright="[0,0,0,0]"
+                data-paddingbottom="[0,0,0,0]"
+                data-paddingleft="[0,0,0,0]"
+                style="z-index: 6; white-space: nowrap; text-transform: left"
+              >
+                We find the best solutions for you, we redesign your home and
+                work places. <br />Discover why over 5,100 home owners trust
+                Initiative.
+              </div>
+
+              <!-- LAYER NR. 3 -->
+              <div
+                class="tp-caption NotGeneric-CallToAction rev-btn"
+                id="slide-1699-layer-75555"
+                data-x="['left','left','left','center']"
+                data-hoffset="['105','107','60','0']"
+                data-y="['middle','middle','middle','middle']"
+                data-voffset="['144','144','144','125']"
+                data-width="none"
+                data-height="none"
+                data-whitespace="nowrap"
+                data-type="button"
+                data-actions='[{"event":"click","action":"scrollbelow","offset":"0px","delay":""}]'
+                data-responsive_offset="on"
+                data-responsive="off"
+                data-frames='[{"from":"y:50px;opacity:0;","speed":1500,"to":"o:1;","delay":1250,"ease":"Power4.easeInOut"},{"delay":"wait","speed":1000,"to":"y:[175%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power2.easeInOut"},{"frame":"hover","speed":"300","ease":"Power1.easeInOut","to":"o:1;rX:0;rY:0;rZ:0;z:0;","style":"c:rgba(255, 255, 255, 1.00);bc:rgba(255, 255, 255, 1.00);bw:1px 1px 1px 1px;"}]'
+                data-textAlign="['left','left','left','left']"
+                data-paddingtop="[15,15,15,15]"
+                data-paddingright="[40,40,40,40]"
+                data-paddingbottom="[15,15,15,15]"
+                data-paddingleft="[40,40,40,40]"
+                style="
+                  z-index: 9;
+                  color: #f7bd1f;
+                  white-space: nowrap;
+                  text-transform: left;
+                  outline: none;
+                  box-shadow: none;
+                  box-sizing: border-box;
+                  -moz-box-sizing: border-box;
+                  -webkit-box-sizing: border-box;
+                  cursor: pointer;
+                  font-family: Oswald;
+                  letter-spacing: 1px;
+                "
+              >
+                VIEW ALL MODELS
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
