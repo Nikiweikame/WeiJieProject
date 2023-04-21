19b8715a4e1eff2d72328250f2e2f53f903cb0c8
Niki wei <frank29580649@gmail.com>
2023-04-21 08:35:20 +0800

    home page

---
+++ WeiJieVue/src/components/HomeContact.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+<style lang="scss" scoped>
+.home-contain {
+  padding: 60px 150px;
+}
+.promise {
+  margin: 0 auto 30px;
+  font-family: "Noto Sans TC";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 35px;
+  /* identical to box height */
+  text-align: center;
+  letter-spacing: 0.2px;
+  color: #373f41;
+}
+.btn-primary {
+  padding: 10px 60px;
+  display: block;
+  margin: 0 auto;
+  width: 200px;
+  font-family: "Noto Sans TC";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 29px;
+  letter-spacing: 0.2px;
+  color: #ffffff;
+}
+</style>
+<template>
+  <div class="home-contain">
+    <p class="promise">對家的規劃有任何專業需求，歡迎與我們聯繫</p>
+    <RouterLink to="/contact" class="btn btn-primary">聯絡崴傑</RouterLink>
+  </div>
+</template>
---
+++ WeiJieVue/src/components/HomeServe.vue
@@ -0,0 +1,101 @@
+<script setup>
+import { ref } from "vue";
+import { RouterLink } from "vue-router";
+const title = ref(["崴傑地產團隊致力於", "房地產及房市相關專業服務"]);
+const ServeItem = ref([
+  {
+    name: "設計規劃",
+    img: "src/images/Servesub.png",
+    path: "/serve",
+  },
+  {
+    name: "工程管理",
+    img: "src/images/Manage.png",
+    path: "/manage",
+  },
+  {
+    name: "土地開發",
+    img: "src/images/Develop.png",
+    path: "/develop",
+  },
+  {
+    name: "物業管家",
+    img: "src/images/Property.png",
+    path: "/property",
+  },
+  {
+    name: "資產標售",
+    img: "src/images/Auction.png",
+    path: "/auction",
+  },
+]);
+</script>
+<style lang="scss" scoped>
+.contain {
+  padding: 60px 150px;
+}
+.title {
+  margin: -30px auto;
+  width: 652px;
+  padding: 30px;
+  p {
+    font-family: "Noto Sans TC";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 35px;
+    text-align: center;
+    letter-spacing: 0.2px;
+    color: #373f41;
+    margin-bottom: 0;
+  }
+}
+.content {
+  margin-top: -30px;
+  justify-content: center;
+}
+.col-4 {
+  margin-top: 30px;
+  position: relative;
+  img {
+    width: 100%;
+  }
+}
+.serve-link {
+  position: absolute;
+  bottom: 0;
+  left: 15px;
+  right: 15px;
+  height: 50px;
+  padding: 5px 20px 10px;
+  font-family: "Noto Sans TC";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 35px;
+  /* identical to box height */
+  letter-spacing: 0.2px;
+
+  background: linear-gradient(
+    90deg,
+    rgba(0, 0, 0, 0.3) 0%,
+    rgba(0, 0, 0, 0) 50%
+  );
+  color: #ffffff;
+}
+</style>
+<template>
+  <div class="contain">
+    <div class="title">
+      <p v-for="text in title">{{ text }}</p>
+    </div>
+    <div class="content row">
+      <div class="col-4" v-for="item in ServeItem">
+        <img :src="item.img" :alt="item.name" />
+        <RouterLink class="serve-link" :to="item.path">{{
+          item.name
+        }}</RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
