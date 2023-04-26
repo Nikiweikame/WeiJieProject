5fd2f2a08936f148167cb5e4bb120e7d84987a92
Niki wei <frank29580649@gmail.com>
2023-04-26 08:30:35 +0800

    serveCard compoment

--- WeiJieVue/src/components/serverCard.vue
+++ WeiJieVue/src/components/serverCard.vue
@@ -9,19 +9,76 @@ defineProps({
   contentText: {
     type: Array,
   },
-  subImg:{
+  subImg: {
     type: String,
-  }
+  },
 });
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+  &__container {
+    padding: 60px 16px 120px;
+    .row {
+      margin-top: -30px;
+    }
+    img {
+      width: 100%;
+    }
+    h4 {
+      font-family: "Noto Sans TC";
+      font-style: normal;
+      font-weight: 500;
+      font-size: 36px;
+      line-height: 52px;
+      letter-spacing: 0.2px;
+      color: #373f41;
+      margin-bottom: 20px;
+    }
+    p {
+      font-family: "Noto Sans TC";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 16px;
+      line-height: 23px;
+      letter-spacing: 0.2px;
+      color: #373f41;
+      margin: 0px;
+      & + p {
+        margin-top: 10px;
+      }
+    }
+  }
+  &__img {
+    margin-top: 30px;
+    text-align: center;
+  }
+  &__text {
+    margin-top: 30px;
+    text-align: center;
+  }
+}
+
+@media screen and (min-width: 1200px) {
+  .card {
+    &__container {
+      padding: 60px 150px 120px;
+    }
+    &__img {
+      text-align: start;
+    }
+    &__text {
+      text-align: start;
+    }
+  }
+}
+</style>
 <template>
-  <div class="serve__content">
+  <div class="card__container">
     <div class="row">
-      <div class="col-12 col-xl-6 serve__card">
+      <div class="col-12 col-xl-6 card__img">
         <img :src="subImg" alt="" />
       </div>
-      <div class="col-12 col-xl-6 serve__card">
+      <div class="col-12 col-xl-6 card__text">
         <h4>{{ nameTitle }}</h4>
         <p v-for="text in contentText" v-if="contentText.length !== 0">
           {{ text }}
